9ec2d6395eb211af9e2fa4c98698e57eca5ddb7c
Christian <christiandecembrana1@gmail.com>
2025-06-21 12:34:55 +0800

    init setup

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+// tailwind.config.js
+export default {
+    content: [
+      "./index.html",
+      "./src/**/*.{js,ts,jsx,tsx}",
+      "./node_modules/@shadcn/ui/**/*.{js,ts,jsx,tsx}"
+    ],
+    theme: {
+      extend: {}
+    },
+    plugins: []
+  }
+  
